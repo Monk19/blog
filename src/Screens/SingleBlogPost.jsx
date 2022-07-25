b85c9b11d0b3147539908b64f431c4dfc9b4529c
rchinta19 <rchinta@appstekcorp.com>
2022-07-25 18:55:32 +0530

    Added viewCount

--- src/Screens/SingleBlogPost.jsx
+++ src/Screens/SingleBlogPost.jsx
@@ -11,27 +11,30 @@ const SingleBlogPost = ({ posts, blogSelect, index }) => {
     backgroundImage: `url(${posts.selectedImage})}`,
   };
   const viewStatus = () => {
-    setViewLikeStatusState({
-      like: allBlogPosts[index].like,
-      view: allBlogPosts[index].view,
-      comment: allBlogPosts[index].comment,
-    });
+    // setViewLikeStatusState({
+    //   like: allBlogPosts[index].like ? allBlogPosts[index].like : 0,
+    //   view: allBlogPosts[index].view ? allBlogPosts[index].view : 0,
+    //   comment: allBlogPosts[index].comment ? allBlogPosts[index].comment : 0,
+    // });
   };
   const kissStupid = (x) => {
-    if (allBlogPosts[index][x] > 0) {
+    console.log(allBlogPosts[index]);
+    if (allBlogPosts[index][x]) {
+      // console.log(allBlogPosts[index][x]);
       setViewLikeStatusState((prev) => ({
         ...prev,
-        [x]: (allBlogPosts[index][x] += 1),
+        x: (viewLikeStatusState.view += 1),
       }));
     } else {
+      allBlogPosts[index][x] = 1;
       setViewLikeStatusState((prev) => ({
         ...prev,
-        [x]: (allBlogPosts[index][x] = 1),
+        [x]: 1,
       }));
     }
   };
   useEffect(() => {
-    viewStatus();
+    // viewStatus();
   }, []);
   return (
     <div style={Styles} className="single-post">
@@ -56,14 +59,18 @@ const SingleBlogPost = ({ posts, blogSelect, index }) => {
           <i
             className="fa-solid fa-eye"
             onClick={() => {
-              console.log(allBlogPosts);
               kissStupid("view");
             }}
           ></i>
           <span>{viewLikeStatusState.view}</span>
         </div>
         <div>
-          <i class="fa-regular fa-thumbs-up" onClick={() => {}}></i>{" "}
+          <i
+            class="fa-regular fa-thumbs-up"
+            onClick={() => {
+              kissStupid("like");
+            }}
+          ></i>{" "}
           <span>{viewLikeStatusState.like}</span>
         </div>
         <div>
