4aa2fc87e0b8cc99d864edd3eb949f923539d042
rchinta19 <rchinta@appstekcorp.com>
2022-07-29 16:02:53 +0530

    Search In Box

--- src/Screens/SearchByGenerComponetHoc.jsx
+++ src/Screens/SearchByGenerComponetHoc.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
 const SearchByGenerComponetHoc = (SomeComponent) => (props) => {
+  console.log({ ...props });
   return (
     <div>
-      <SomeComponent />
+      <SomeComponent {...props} />
     </div>
   );
 };
